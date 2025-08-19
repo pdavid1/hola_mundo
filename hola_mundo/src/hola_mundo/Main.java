d366b67d67d67b55f3bab92e40023516b0c6c3d3
Paul <pdavidae@gmail.com>
2025-08-19 16:54:48 -0400

    clase ArrayList

--- hola_mundo/src/hola_mundo/Main.java
+++ hola_mundo/src/hola_mundo/Main.java
@@ -1,11 +1,18 @@
 package hola_mundo;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) { 
-        Almacen <String>almacen= new Almacen();
-        almacen.setDato("Hola");
-
-        String dato = almacen.getDato();
+        int[] array = new int[3];
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        lista.add(20);
+        lista.add(40);
+        lista.add(943);
+        lista.add(32);
+        System.out.println(lista);
+        lista.remove(0);
+        System.out.println(lista);
    }
 }
