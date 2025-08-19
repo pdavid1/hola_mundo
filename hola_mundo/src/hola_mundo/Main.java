e56bf502410cdcdf92f5b8c19caff9db599cc782
Paul <pdavidae@gmail.com>
2025-08-19 17:16:32 -0400

    clase Lanzar excepciones con throw

--- hola_mundo/src/hola_mundo/Main.java
+++ hola_mundo/src/hola_mundo/Main.java
@@ -5,11 +5,15 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) { 
+        int posicion = 3;
         int[] array = new int[3];
         try {
-            array[2] = 1;
+            if (posicion > array.length - 1) {
+                throw new PosicionIncorrectaException();
+            }
+            array[posicion] = 3;
             System.out.println("Todo ha salido bien");
-        } catch (IllegalArgumentException e) {
+        } catch (PosicionIncorrectaException e) {
             System.out.println("Ha habido un error con el array");
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Has accedido a una posicion incorrecta del array");
